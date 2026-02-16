ce6342547b574ed4af04ac6cd3467d8e41d62d7c
dattatray <dattapanchal464@gmail.com>
2026-02-16 15:07:18 +0530

    implement cart functionality with controller, model and routes

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import { connectDB } from './database/db.js';
 import userRoutes from './routes/user.routes.js';
 import productRoutes from './routes/product.routes.js';
+import cartRoutes from './routes/cart.routes.js';
+import { isAuthenticated } from './middlewares/isAuthenticated.js';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3200;
@@ -16,6 +18,7 @@ app.use(cors({
 
 app.use("/api/v1/user",userRoutes);
 app.use("/api/v1/product",productRoutes);
+app.use("/api/v1/cart",isAuthenticated,cartRoutes);
 connectDB();
 
 app.listen(port,()=>{
